383755d8ab823c268c96fa8e95d13894ab6dbc2b
sudip <sudip220701@gmail.com>
2023-03-06 14:52:47 +0530

    Adding Villa in views  with create,update,index and delete cshtml files

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -16,5 +16,59 @@ namespace MagicVilla_Web.Controllers{
       }
       return View(list);
     }
+    public async Task<IActionResult>CreateVilla(){
+      
+      return View();
+    }
+    [HttpPost]
+    [ValidateAntiForeignToken]
+    public async Task<IActionResult>CreateVilla(VillaCreateDTO model){
+      if(ModelState.IsValid){
+        var response =await _villaService.CreateAsync<APIResopnse>(model);
+        if(response!=null && response.IsSuccess){
+        return RedirectToAction(nameof(IndexVilla));
+      }
+      }
+      return View(model);
+    }
+    public async Task<IActionResult>UpdateVilla(int villaId){
+      var response =await _villaService.GetAsync<APIResopnse>(villaId);
+        if(response!=null && response.IsSuccess){
+        VillaDTO model=JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+        return View(_mapper.Map<VillaUpdateDTO>(model));
+      }
+      
+      return NotFound();
+    }
+    [HttpPost]
+    [ValidateAntiForeignToken]
+    public async Task<IActionResult>UpdateVilla(VillaUpdateDTO model){
+      if(ModelState.IsValid){
+        var response =await _villaService.UpdateAsync<APIResopnse>(model);
+        if(response!=null && response.IsSuccess){
+        return RedirectToAction(nameof(IndexVilla));
+      }
+      }
+      return View(model);
+    }
+    public async Task<IActionResult>DeleteVilla(int villaId){
+      var response =await _villaService.GetAsync<APIResopnse>(villaId);
+        if(response!=null && response.IsSuccess){
+        VillaDTO model=JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+        return View(model);
+      }
+      
+      return NotFound();
+    }
+    [HttpPost]
+    [ValidateAntiForeignToken]
+    public async Task<IActionResult>UpdateVilla(VillaDTO model){
+      
+        var response =await _villaService.DeleteAsync<APIResopnse>(model.Id);
+        if(response!=null && response.IsSuccess){
+        return RedirectToAction(nameof(IndexVilla));
+      }
+      return View(model);
+    }
   }
 }
